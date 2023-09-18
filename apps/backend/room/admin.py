bffdc85177867a61ed7c679c476680380111b406
Aakash subedi <aakkashsubedi@gmail.com>
2023-09-18 11:23:08 +0545

    ok

--- apps/backend/room/admin.py
+++ apps/backend/room/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Room)
+
+class RooomAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'type', 'status', 'position']
+    search_fields = ['title', 'price', 'type']
+    list_display_links = ['title', 'price', ]
+    list_filter = ['type']
+
+
+admin.site.register(Room, RooomAdmin)
 admin.site.register(RoomType)
